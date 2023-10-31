5485583dedb0ce8fe4ffc5dedf81aae69c397c8e
rjesu <rjesu@DESKTOP-GS9P98R>
2023-10-30 20:49:04 -0600

    Avance #1

---
+++ src/main/java/com/KisaneStore/KisaneStoreApplication.java
@@ -0,0 +1,13 @@
+package com.KisaneStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisaneStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KisaneStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KisaneStore/KisaneStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KisaneStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KisaneStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
